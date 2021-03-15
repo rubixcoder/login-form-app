429e18aba150956229f60a6615eacd7dfbf01b01
Surbhi Harsh <surbhi.harsh@xebia.com>
2021-03-15 16:40:55 +0530

    added auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent, canActivate: []},
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
   { path: 'login', component: LoginComponent},
 
   // otherwise redirect to login
